51f9a07f5ab6792e669ceaf0433c92315dcfcaa1
Sangsaka Wira <sangsakawira@gmail.com>
2021-04-16 11:11:42 +0700

    Change Live Status

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import React, { useEffect, useState } from "react";
+import { Form, Button, Input } from "antd";
 
 export default function App() {
   let [geo, setGeo] = useState({});
@@ -14,13 +15,30 @@ export default function App() {
     );
   }, []);
 
+  const onFinish = (values) => {
+    console.log(values);
+  };
+
   return (
     <div className="App">
       <h1>GPS Data Xuro Tech</h1>
+      <h1 style={{ color: "red" }}>Not Live</h1>
       <h2>Latitude: {geo.latitude}</h2>
       <h2>Longitude: {geo.longitude}</h2>
       {/* https://www.google.com/maps/search/?api=1&query=36.26577,-92.54324 */}
+      <Form onFinish={onFinish}>
+        <Form.Item name="unique_id" placeholder="Unique Id">
+          <Input></Input>
+        </Form.Item>
+        <br />
+        <Button type="primary" htmlType="submit">
+          Set Live
+        </Button>
+      </Form>
+      <br />
+      <br />
       <a
+        target="_blank"
         href={`https://www.google.com/maps/search/?api=1&query=${geo.latitude},${geo.longitude}`}
       >
         Get to Location
